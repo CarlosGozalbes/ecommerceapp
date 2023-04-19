aed2986930ff2c67416f988565ab9291c7553135
CarlosGozalbes <carlosfgmendoza@gmail.com>
2023-04-19 19:16:24 +0200

    admin page finished

--- utils/dataURItoBlob.js
+++ utils/dataURItoBlob.js
@@ -2,8 +2,8 @@ export default function dataURItoBlob(dataURI) {
   // convert base64/URLEncoded data component to raw binary data held in a string
   var byteString;
   if (dataURI.split(",")[0].indexOf("base64") >= 0)
-    byteString = atob(dataURI.split(",")[1]);
-  else byteString = unescape(dataURI.split(",")[1]);
+    byteString = window.atob(dataURI.split(",")[1]);
+  else byteString = decodeURIComponent(dataURI.split(",")[1]);
 
   // separate out the mime component
   var mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
